76f92b1fdb90420959932da8dedfddcf505abc2b
aantariksh <71594174+aantariksh@users.noreply.github.com>
2021-07-10 17:10:32 +0530

    Admin display bills (#9)
    * Add option to mark paid bills
    
    * Update tertiary operator to OR operator
    
    Co-authored-by: sahil-777 <turbothunderboltz@gmail.com>

--- Admin/assets/firebase/all-bills.js
+++ Admin/assets/firebase/all-bills.js
@@ -13,48 +13,94 @@ firebase.auth().onAuthStateChanged((user) => {
  */
 async function showAllStoresAllBills() {
 
-    const snapshot = await firebase.database().ref(`Teqmo/Stores`).once('value')
-    const data = snapshot.val()
+  const snapshot = await firebase.database().ref(`Teqmo/Stores`).once('value')
+  const data = snapshot.val()
 
-    var dataSet = []
-    
-    jQuery.each(data, function(storeUID, details) {
+  let sortedByWeeks = {};
+  jQuery.each(data, function (storeUID, details) {
     if (details) {
-        jQuery.each(details.payment.weeks, function(weekNum, weekDetails) {
-            if (weekDetails && weekNum!="start") {
-                let storeName = details.details.storeName ? details.details.storeName : 'No Store Name';
-                let startDate = getDateFromWeek(weekNum, 0);
-                let endDate = getDateFromWeek(weekNum, 1);
-                let billStatus = weekDetails.billStatus;
-                let total = weekDetails.sales ? weekDetails.sales : 'N/A';
-                var statusLine
-                if(billStatus==0){
-                    statusLine = `<span class="badge badge-soft-secondary">
-                                    <span class="legend-indicator bg-secondary"></span>Not Generated
-                                  </span>`
-                }
-                else if (billStatus==1){
-                    statusLine = `<span class="badge badge-soft-warning">
-                                    <span class="legend-indicator bg-warning"></span>Pending
-                                  </span>`
-                }
-                else {
-                    statusLine = `<span class="badge badge-soft-success">
-                                    <span class="legend-indicator bg-success"></span>Paid
-                                  </span>`
-                }
-                let invoice = `<a class="btn btn-sm btn-white" href="invoice.html?storeUID=${storeUID}&weekID=${weekNum}">
-                <i class="tio-receipt-outlined mr-1"></i> Invoice</a>`
-
-                var temp = [storeName,startDate,endDate,statusLine,total,invoice]
-                dataSet.unshift(temp)
-                }
-            })
+      jQuery.each(details.payment.weeks, function (weekNum, weekDetails) {
+        if (weekDetails && weekNum != "start") {
+          let storeData = {
+            "storeName": details.details.storeName,
+            "billStatus": weekDetails.billStatus, // Not checking if data is undefined or not, as It will be checked later
+            "weeklyCommission": weekDetails.commission
+          };
+          if (sortedByWeeks[weekNum]) {
+            sortedByWeeks[weekNum][storeUID] = storeData;
+          } else {
+            sortedByWeeks[weekNum] = {
+              [storeUID]: storeData
+            };
+          }
         }
-    })
-    updateDataTable(dataSet)
+      })
+    }
+  })
+
+  let dataSet = []
+  jQuery.each(sortedByWeeks, function (weekNum, allStores) {
+    if (allStores) {
+      jQuery.each(allStores, function (storeUID, storeData) {
+        let storeName = storeData.storeName || 'No Store Name';
+        let startDate = getDateFromWeek(weekNum, 0);
+        let endDate = getDateFromWeek(weekNum, 1);
+        let billStatus = storeData.billStatus || -1;
+        let total = storeData.weeklyCommission || 'N/A';
+        var statusLine
+        let mark = ``
+        if (billStatus == 0) {
+          statusLine = `<span class="badge badge-soft-secondary">
+                                <span class="legend-indicator bg-secondary"></span>Not Generated
+                              </span>`
+        } else if (billStatus == 1) {
+          statusLine = `<span id="${storeUID}-${weekNum}Status" class="badge badge-soft-warning">
+                                <span class="legend-indicator bg-warning"></span>Pending
+                              </span>`
+
+          mark = `<button id="${storeUID}-${weekNum}" class="btn btn-sm btn-white" onclick="updateBillStatus(this.id)">
+              Paid</button>`
+
+        } else if (billStatus == 2) {
+          statusLine = `<span class="badge badge-soft-success">
+                                <span class="legend-indicator bg-success"></span>Paid
+                              </span>`
+        }
+        else{
+          statusLine = `<span class="badge">
+                                <span class="legend-indicator"></span>N/A
+                              </span>`
+        }
+
+        let invoice = `<a class="btn btn-sm btn-white" href="invoice.html?storeUID=${storeUID}&weekID=${weekNum}">
+            <i class="tio-receipt-outlined mr-1"></i> Invoice</a>`
+            
+        var temp = [storeName, startDate, endDate, statusLine, mark, total, invoice]
+        dataSet.unshift(temp)
+      })
+    }
+  })
+  updateDataTable(dataSet)
 }
 
+function updateBillStatus(btnID) {
+  let storeUID = btnID.split('-')[0]
+  let weekNum = btnID.split('-')[1]
+  firebase.database().ref(`Teqmo/Stores/${storeUID}/payment/weeks/${weekNum}`).update({
+    'billStatus': 2 //Paid
+  }).then(() => {
+    document.getElementById(btnID).innerHTML = `Paid <i class="tio-done mr-1"></i>`
+    document.getElementById(btnID + 'Status').innerHTML = `<span class="badge badge-soft-success">
+      <span class="legend-indicator bg-success"></span>Paid
+      </span>`
+  }).catch((error) => {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: `Something went bad, Please try again`,
+    })
+  });
+}
 
 // INITIALIZATION OF DATATABLES
 function updateDataTable(dataSet) {
@@ -65,6 +111,7 @@ function updateDataTable(dataSet) {
             { title: "Start Date" },
             { title: "End Date" },
             { title: "Payment Status"},
+            { title: "Update Status"},
             { title: "Commission"},
             { title: "Invoice" }
         ],
--- Admin/assets/firebase/store-details.js
+++ Admin/assets/firebase/store-details.js
@@ -20,17 +20,17 @@ async function showStoresDetails(){
  * @param {object} storeDetails object available at 'Teqmo/Stores/UID/details'
  */
 function storeOwnerInfo(storeDetails){
-    let ownerName = storeDetails.ownerName ? storeDetails.ownerName : 'No Name'
+    let ownerName = storeDetails.ownerName || 'No Name'
 
-    let email = storeDetails.email ? storeDetails.email : 'Email not available'
-    let phone = storeDetails.phone ? storeDetails.phone : 'No Data'
+    let email = storeDetails.email || 'Email not available'
+    let phone = storeDetails.phone || 'No Data'
     let contactInfo =` <li><i class="tio-online mr-2"></i>${email}</li>
             <li><i class="tio-android-phone-vs mr-2"></i>${phone}</li>`;
     
-    let city = storeDetails.city ? storeDetails.city : 'No City'
-    let state = storeDetails.state ? storeDetails.state : 'No Sate'
-    let companyName = storeDetails.companyName ? storeDetails.companyName : 'No Company Name'
-    let storeName = storeDetails.storeName ? storeDetails.storeName : 'No Store Name'
+    let city = storeDetails.city || 'No City'
+    let state = storeDetails.state || 'No Sate'
+    let companyName = storeDetails.companyName || 'No Company Name'
+    let storeName = storeDetails.storeName || 'No Store Name'
     let storeInfo = `<li><i class="tio-user-outlined mr-2"></i>${storeName}</li>
                     <li><i class="tio-briefcase-outlined mr-2"></i>${companyName}</li>
                     <li><i class="tio-city mr-2"></i>${city}, ${state}</li>`;
@@ -46,8 +46,8 @@ function storeOwnerInfo(storeDetails){
  * @param {object} storePayment object available at 'Teqmo/Stores/UID/payment'
  */
 function storeReportCard(storePayment){
-    const totalSales = storePayment.totalSales ? storePayment.totalSales : 0
-    const totalCommission = storePayment.totalCommission ? storePayment.totalCommission : 0
+    const totalSales = storePayment.totalSales || 0
+    const totalCommission = storePayment.totalCommission || 0
     const totalProfit = totalSales - totalCommission
     let totalCount =  0
     jQuery.each(storePayment.weeks, function(weekNum, details) {
@@ -75,7 +75,6 @@ async function storeBillDetails(weeks,storeUID){
     if(weeks){
         jQuery.each(weeks, function(weekNum, details) {
             if (details && weekNum!='start') {
-                // console.log(weekNum, details)
                 let startDate = getDateFromWeek(weekNum, 0); //From Common function's File
                 let endDate = getDateFromWeek(weekNum, 1);
                 let billStatus = billStatusArray[0], billIconClass = billStatusClasses[0]
@@ -83,17 +82,23 @@ async function storeBillDetails(weeks,storeUID){
                 if (details.billStatus) {
                     billStatus = billStatusArray[details.billStatus];
                     billIconClass = billStatusClasses[details.billStatus];
-                    sales = details.sales ? details.sales : 'N/A';
-                    commission = details.commission ? details.commission : 'N/A';
+                    sales = details.sales || 'N/A';
+                    commission = details.commission || 'N/A';
                 }
 
-                let billStatusHTML = `<span class="badge badge-soft-${billIconClass}">
+                let billStatusHTML = `<span id="${storeUID}-${weekNum}Status" class="badge badge-soft-${billIconClass}">
                                     <span class="legend-indicator bg-${billIconClass}"></span>${billStatus}
                                     </span>`;
+                
+                let mark = ``
+                if (details.billStatus == 1) {
+                    mark = `<button id="${storeUID}-${weekNum}" class="btn btn-sm btn-white" onclick="updateBillStatus(this.id)">
+                    Paid</button>`
+                }
                 let invoice = `<a class="btn btn-sm btn-white" href="invoice.html?storeUID=${storeUID}&weekID=${weekNum}">
                 <i class="tio-receipt-outlined mr-1"></i> Invoice</a>`
                 
-                let row = [startDate, endDate, billStatusHTML, sales, commission, invoice];
+                let row = [startDate, endDate, billStatusHTML, mark, sales, commission, invoice];
                 billDetailsData.unshift(row);   //Adding JSON at beginning
             }
         });
@@ -102,6 +107,25 @@ async function storeBillDetails(weeks,storeUID){
         console.log('No week exists');
     }
     updateDataTable(billDetailsData);
+}
+
+function updateBillStatus(btnID) {
+    let storeUID = btnID.split('-')[0]
+    let weekNum = btnID.split('-')[1]
+    firebase.database().ref(`Teqmo/Stores/${storeUID}/payment/weeks/${weekNum}`).update({
+        'billStatus': 2 //Paid
+    }).then(() => {
+        document.getElementById(btnID).innerHTML = `Paid <i class="tio-done mr-1"></i>`
+        document.getElementById(btnID + 'Status').innerHTML = `<span class="badge badge-soft-success">
+        <span class="legend-indicator bg-success"></span>Paid
+        </span>`
+    }).catch((error) => {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: `Something went bad, Please try again`,
+        })
+    });
 }
 
 /**
@@ -121,6 +145,7 @@ function updateDataTable(dataSet){
           { title: "Start Date" },
           { title: "End Date" },
           { title: "Payment" },
+          { title: "Update Status" },
           { title: "Sales" },
           { title: "Commission" },
           { title: "Invoice" }
